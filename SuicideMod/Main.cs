def5f097fab617345990192aa4406b440b84c33b
Thomas Margraff <thomaspmar@protonmail.com>
2022-01-12 12:23:13 -0800

    Updated for public release

---
+++ SuicideMod/Main.cs
@@ -0,0 +1,16 @@
+﻿using OWML.ModHelper;
+using UnityEngine.InputSystem;
+
+namespace SuicideMod
+{
+    public class Main : ModBehaviour
+    {
+        private void Update()
+        {
+            if (!Keyboard.current[Key.K].wasPressedThisFrame)
+                return;
+
+            FindObjectOfType<DeathManager>()?.KillPlayer(DeathType.Default);
+        }
+    }
+}
